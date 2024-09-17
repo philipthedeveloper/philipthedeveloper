fc553a82d102da054b79c23591f3fae05c7612e5
philipthedeveloper <88097351+philipthedeveloper@users.noreply.github.com>
2024-09-17 08:48:33 +0100

    Update my story

--- src/constants/my-story-data.js
+++ src/constants/my-story-data.js
@@ -1,53 +1,64 @@
+// const myStory = [
+//   {
+//     id: "story-part-1",
+//     icon: "next",
+//     to: "#story-part-2",
+//     content:
+//       "My unwavering passion for computers and how they work didn’t kick-start until the end of my first year in uni. At that time, I was naive and had a vague understanding of anything that involves writing computer program.",
+//   },s
+//   {
+//     id: "story-part-2",
+//     icon: "next",
+//     to: "#story-part-3",
+//     content:
+//       "I decided to free myself from the ignorance that had plagued me for a very long time. So, during my preparation for sophomore I starting my learning journey. ",
+//   },
+//   {
+//     id: "story-part-3",
+//     icon: "next",
+//     to: "#story-part-4",
+//     content:
+//       "More than 3 years have passed now and over the years I have picked up various web technologies, framework and design practice and patterns as a result of which I amazed a vast knowledge of programming. I have build solutions for small businesses, start-ups, and companys.",
+//   },
+//   {
+//     id: "story-part-4",
+//     icon: "end",
+//     to: "#work-experience",
+//     content:
+//       "Of course, above all, my greatest feat was setting foot into the world of problem solving which lead me to the understanding that it isn’t enough to learn how to build something but that more importantly building something that solves a problem.",
+//   },
+// ];
+
+// export default myStory;
 const myStory = [
   {
     id: "story-part-1",
     icon: "next",
     to: "#story-part-2",
     content:
-      "My unwavering passion for computers and how they work didn’t kick-start until the end of my first year in uni. At that time, I was naive and had a vague understanding of anything that involves writing computer program.",
+      "My passion for computers and how they work didn’t begin until the end of my first year at university. At that time, I was naive and had only a vague understanding of programming.",
   },
-  // {
-  //   id: "story-part-2",
-  //   icon: "next",
-  //   to: "#story-part-3",
-  //   content:
-  //     "Anytime I saw any cool stuff either on the web or within an application, magic was the first word that cross my mind.  I had seen a few of my mates and colleagues write basic program and it felt like they had superpowers whenever they did.",
-  // },
   {
     id: "story-part-2",
     icon: "next",
     to: "#story-part-3",
     content:
-      "I decided to free myself from the ignorance that had plagued me for a very long time. So, during my preparation for sophomore I starting my learning journey. ",
+      "I decided to break free from the ignorance I had carried for a long time. During my preparation for sophomore year, I started my learning journey.",
   },
   {
     id: "story-part-3",
     icon: "next",
     to: "#story-part-4",
     content:
-      "More than 3 years have passed now and over the years I have picked up various web technologies, framework and design practice and patterns as a result of which I amazed a vast knowledge of programming. I have build solutions for small businesses, start-ups, and companys.",
+      "Over the past four years, I’ve picked up various web technologies, frameworks, design practices, and patterns, amassing a vast knowledge of programming. I have built solutions for small businesses, start-ups, and companies.",
   },
-  // {
-  //   id: "story-part-5",
-  //   icon: "next",
-  //   to: "#story-part-6",
-  //   content:
-  //     "Of course, above all, my greatest feat was setting foot into the world of problem solving which lead me to the understanding that it isn’t enough to learn how to build something but that more importantly building something that solves a problem.",
-  // },
   {
     id: "story-part-4",
     icon: "end",
     to: "#work-experience",
     content:
-      "Of course, above all, my greatest feat was setting foot into the world of problem solving which lead me to the understanding that it isn’t enough to learn how to build something but that more importantly building something that solves a problem.",
+      "Above all, my greatest accomplishment was entering the world of problem-solving, which led me to realize that it’s not enough to simply build something; it’s far more important to build something that solves a real problem.",
   },
-  // {
-  //   id: "story-part-6",
-  //   icon: "end",
-  //   to: "#work-experience",
-  //   content:
-  //     "Conclusively, I want to say thank you for taking your time to read this far. I am happy for how far I have come and I hope you’re too. I am looking forward with all preparedness and anticipation where this path will lead.",
-  // },
 ];
 
 export default myStory;
